52846c9444a168e6c99fea247d7586c0360191dd
novlau <70096284+novlau@users.noreply.github.com>
2020-08-23 20:45:51 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -19,30 +19,6 @@ async def ping(ctx):
 
     
     
-#coding:UTF-8
-import discord
-from discord.ext import tasks
-from datetime import datetime 
-
-TOKEN = "**********" #トークン
-CHANNEL_ID = ********** #チャンネルID
-# 接続に必要なオブジェクトを生成
-client = discord.Client()
-
-# 60秒に一回ループ
-@tasks.loop(seconds=60)
-async def loop():
-    # 現在の時刻
-    now = datetime.now().strftime('%H:%M')
-    if now == '07:00':
-        channel = client.get_channel(CHANNEL_ID)
-        await channel.send('おはよう')  
-
-#ループ処理実行
-loop.start()
-# Botの起動とDiscordサーバーへの接続
-client.run(TOKEN)
-    
     
 
 bot.run(token)
